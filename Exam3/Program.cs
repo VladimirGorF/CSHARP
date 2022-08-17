1b17baed1cfd4dfd892bf21cb5419c367d7ee3e6
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-08-17 20:41:23 +0300

    Random

--- Exam3/Program.cs
+++ Exam3/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
-int numberA = 10;
-int numberB = 5;
-Console.WriteLine(numberA / numberB);
+int numberA = new Random().Next(1, 10); //1, 2, 3 ... 9//
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
    
