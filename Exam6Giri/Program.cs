c89a4db8773b4ca215943826f23ea60b1d8433f6
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-08-18 08:43:15 +0300

    i did giri

---
+++ Exam6Giri/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 1;
+int c = 1;
+int d = 7;
+int e = 1;
+
+int max = a;
+
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(e>max) max = e;
+
+Console.Write("max = ");
+
+
+Console.WriteLine(max);
